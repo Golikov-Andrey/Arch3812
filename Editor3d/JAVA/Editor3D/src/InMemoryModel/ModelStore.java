eaee68f45e2e520cff9ad1ad9e9eb0e155863cec
Golikov-Andrey <newsc2@yandex.ru>
2023-11-17 22:14:04 +0300

    solution from sem 2 arch software (reward app)

--- Editor3d/JAVA/Editor3D/src/InMemoryModel/ModelStore.java
+++ Editor3d/JAVA/Editor3D/src/InMemoryModel/ModelStore.java
@@ -7,6 +7,7 @@ import ModelEliments.Camera;
 import ModelEliments.Flash;
 import ModelEliments.PoligonModel;
 import ModelEliments.Scene;
+import ModelEliments.Texture;
 
 public class ModelStore implements IModelChanger {
     public List<PoligonModel> models;
@@ -15,13 +16,23 @@ public class ModelStore implements IModelChanger {
     public List<Camera> cameras;
     private IModelChangeObserver[] changeObservers;
 
-    public ModelStore(IModelChangeObserver[] changeObservers) {
+    public ModelStore(IModelChangeObserver[] changeObservers) throws Exception {
         this.changeObservers = changeObservers;
 
         this.models = new ArrayList<>();
         this.scenes = new ArrayList<>();
         this.flashes = new ArrayList<>();
         this.cameras = new ArrayList<>();
+
+        List<Texture> textures = new ArrayList<>();
+        this.models.add(new PoligonModel(textures));
+
+        this.flashes.add(new Flash());
+        this.cameras.add(new Camera());
+
+        this.scenes.add(new Scene(0, models, flashes, cameras));
+
+
     }
 
     public Scene getScene(int id)
